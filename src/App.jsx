2253dfafe3833585d221b577a3705caed8128198
Adrianaortiz00 <a0207056@gmail.com>
2024-06-13 11:19:21 +0200

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+
 import ProductDetails from "./pages/product-details/ProductDetails";
+import Footer  from "./components/footer";
 
 export default function App() {
   return (
     <div>
       <ProductDetails />
-    </div>
-  );
-}
+      <Footer />
+      </div>
+)}
