46fa61953421691f4b293d87ec817a2301680824
Lynn Poh <pohwlynn@gmail.com>
2024-06-13 10:20:16 +0200

    added imports to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './styles/app.scss';
 import './styles/delivery.scss';
+import ProductDetails from './pages/product'
 import Delivery from './components/delivery.jsx';
 //import ProductContainer from './components/productContainer.jsx';
 
 function App() {
     return (
+        <>
+        <div>
+          <ProductDetails />
+        </div>
         <div className='cartContainer'>
             <Delivery/>
         </div>
+        </>
     )
 }
 
