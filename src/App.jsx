7b8cf1e49989238cd04404b1f2cdfe9505c9d44e
Lynn Poh <pohwlynn@gmail.com>
2024-06-13 09:26:31 +0200

    created delivery component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
+import './styles/app.scss';
+import './styles/delivery.scss';
+import Delivery from './components/delivery.jsx';
+//import ProductContainer from './components/productContainer.jsx';
+
+function App() {
     return (
-        <div>
-            <h1>Funciona</h1>
+        <div className='cartContainer'>
+            <Delivery/>
         </div>
-    );
+    )
 }
+
+export default App
---
+++ src/components/productContainer.jsx
@@ -0,0 +1,15 @@
+import Delivery from 'src/components/delivery.jsx';
+
+const ProductContainer = () => {
+    return (
+        <div className='productContainer'>
+            <Delivery/>
+        </div>
+
+    )
+}
+
+
+
+
+export default ProductContainer;
