b9236a677ff9a8b6d48654058f05415198954116
LauraGDev <laura.gilcara@gmail.com>
2024-06-13 21:55:32 +0200

    create navigation links with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Header from './components/nav/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/nav/Header";
 import "./App.css";
+import { Earbuds } from "./pages/Earbuds";
+import { Smartwatch } from "./pages/Smartwatch";
+import { Cart } from "./pages/Cart";
 
 export default function App() {
     return (
         <div className="App">
-            <Header />
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Earbuds />} />
+                    <Route path="/earbuds" element={<Earbuds />} />
+                    <Route path="/watches" element={<Smartwatch />} />
+                    <Route path="/cart" element={<Cart />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
+
+//export default App;
