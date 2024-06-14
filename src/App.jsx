00c6329817fa690032a3fb184695c367071fd58a
Lynn Poh <pohwlynn@gmail.com>
2024-06-14 14:39:50 +0200

    WIP arreglar cartProductcontainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './styles/variables.scss';
 import './styles/app.scss';
 import './components/cart/delivery.scss';
-import './components/cart/cart.scss';
+import './pages/cart.scss';
 import './components/cart/cartProdContainer.scss';
 //import ProductDetails from './pages/product-details/ProductDetails.jsx'
 //import Delivery from './components/cart/Delivery.jsx';
 import CartProdContainer from './components/cart/CartProdContainer.jsx';
-import Cart from './components/cart/Cart.jsx';
+import Cart from './pages/Cart.jsx';
 
 function App() {
     return (
